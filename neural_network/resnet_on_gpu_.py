2e9c274b41b25257faac32ec024eeb1088b08a88
Arty_Solomko <artysolomko@gmail.com>
2021-08-25 12:51:13 +0300

    change_proj

--- neural_network/resnet_on_gpu_.py
+++ neural_network/resnet_on_gpu_.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Conv2D, Flatten, Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.preprocessing.image import ImageDataGenerator 
@@ -27,7 +27,7 @@ def load_train(path):
         target_size=(150, 150),
         class_mode='sparse',
         subset='training',
-        batch_size=16,
+        batch_size=32,
         seed=12345))
     
 
@@ -35,27 +35,25 @@ def load_train(path):
 
 
 def create_model(input_shape):
-    path_weights = '/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
-    backbone = ResNet50(input_shape=(150, 150, 3),
-                    weights=path_weights,
-                    include_top=False) 
-    backbone.trainable = True
+ 
+    backbone = ResNet50(input_shape=input_shape,
+                    weights='/datasets/keras_models/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                    include_top=False)
+    
+    #backbone.trainable = True
 
     model = Sequential()
     model.add(backbone)
     model.add(GlobalAveragePooling2D())
     model.add(Dense(12, activation='softmax')) 
 
-    optimizer = Adam(lr=0.001) 
+    optimizer = Adam(lr=0.0005) 
     model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy',
                   metrics=['acc'])
-    
-
-
     return model
 
 
-def train_model(model, train_data, test_data, epochs=5, batch_size=None,
+def train_model(model, train_data, test_data, epochs=3, batch_size=None,
                steps_per_epoch=None, validation_steps=None):
   
     train_datagen_flow = train_data
